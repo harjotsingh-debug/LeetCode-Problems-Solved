aaa827e6c81578b7fc52908d64cf851d1e71535b
Harjot <harjotone@gmail.com>
2026-07-31 17:16:26 +0530

    Time: 49 ms (19.01%), Space: 36.7 MB (39.58%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int f(int i,vector<int>& coins,int t){
+        if(t==0) return 1;
+        if(i==coins.size()-1) return t%coins[coins.size()-1]==0;
+        if(dp[i][t]!=-1) return dp[i][t];
+        int ntake=f(i+1,coins,t);
+        int take=0;
+        if(coins[i]<=t) take=f(i,coins,t-coins[i]);
+
+        return dp[i][t]=take+ntake;
+    }
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        dp.resize(n,vector<int>(amount+1,-1));
+       return f(0,coins,amount);
+    }
+};
