4958e91ce5daefd371bcaf3452b729abcf47bba9
Harjot <harjotone@gmail.com>
2026-08-25 14:29:46 +0530

    Time: 0 ms (100%), Space: 8.5 MB (79.79%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
-
     vector<int> dp;
 
     int solve(int i, string &s) {
 
-        // Reached the end
         if(i == s.size())
             return 1;
 
-        // Already calculated
         if(dp[i] != -1)
             return dp[i];
 
-        // A number starting with 0 is invalid
-        if(s[i] == '0')
-            return dp[i] = 0;
+        // choice 1: take one digit
+        int one = 0;
 
-        int ways = 0;
+        if(s[i] != '0') {
+            one = solve(i + 1, s);
+        }
 
-        // Take one digit
-        ways += solve(i + 1, s);
+        // choice 2: take two digits
+        int two = 0;
 
-        // Take two digits
         if(i + 1 < s.size()) {
 
-            int num = (s[i]-'0') * 10 + (s[i+1]-'0');
+            int num = (s[i] - '0') * 10
+                    + (s[i + 1] - '0');
 
             if(num >= 10 && num <= 26) {
-                ways += solve(i + 2, s);
+                two = solve(i + 2, s);
             }
         }
 
-        return dp[i] = ways;
+        return dp[i] = one + two;
     }
 
     int numDecodings(string s) {
